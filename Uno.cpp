f7a616bd69425a259129d7d9325df8428cb75fd0
Maxwell Wang <90732327+mwang840@users.noreply.github.com>
2021-12-31 18:38:59 -0500

    Add files via upload

---
+++ Uno.cpp
@@ -0,0 +1,18 @@
+/*
+ * Uno.cpp
+ *
+ *  Created on: Dec 29, 2021
+ *      Author: kabut
+ */
+
+#include "Uno.hpp"
+
+Uno::Uno() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Uno::~Uno() {
+	// TODO Auto-generated destructor stub
+}
+
