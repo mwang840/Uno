57066f2b1d1a68b91c2a4f82460c94c2dce75124
Maxwell Wang <90732327+mwang840@users.noreply.github.com>
2021-12-31 18:40:33 -0500

    Add files via upload

---
+++ Uno.hpp
@@ -0,0 +1,20 @@
+/*
+ * Uno.hpp
+ *
+ *  Created on: Dec 29, 2021
+ *      Author: kabut
+ */
+
+#ifndef UNO_HPP_
+#define UNO_HPP_
+
+class Uno {
+	//Player *you;
+	//Deck *hand;
+public:
+	Uno();
+	void startGame();
+	virtual ~Uno();
+};
+
+#endif /* UNO_HPP_ */
