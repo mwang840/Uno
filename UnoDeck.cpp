fa4b057b225820d1d3b346944ee12f1b8ad181ff
Maxwell Wang <90732327+mwang840@users.noreply.github.com>
2022-01-02 21:34:57 -0500

    Update UnoDeck.cpp

--- UnoDeck.cpp
+++ UnoDeck.cpp
@@ -14,21 +14,76 @@ using namespace std;
 
 //Takes in account the whole uno pile
 UnoDeck::UnoDeck() {
+	//Player does not have a hand yet
+	player->hand = NULL;
+	totalCards = 108;
+	totalDiscarded = 0;
 	string colors[] = {"blue", "red", "green", "yellow"};
 	int numbers [] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	string events[] = {"skip", "reverse", "drawTwo", "drawFour", "wild"};
-	/*for(int i = 0; i < sizeof(numbers); ++i){
-
-	}
-
-	*/
-
 	this->totalCards-=7;
 	this->hand->cardNum +=7;
 }
 
 //Skips the player and check to see if the next's next pointer is not null
-UnoCard *UnoDeck::skip(){
+UnoPlayer *UnoDeck::skip(UnoPlayer *person){
+	UnoPlayer *player = person->prev;
+	//Checks to see if there are more than two players
+	if(player->next->next != player){
+		player = person->next;
+		return player;
+	}
+	//In the case there are two players, easy one
+	else if(player->next->next == player){
+		player->last = player->prev;
+		player->last->next = player;
+		return player;
+	}
+	//Else there is only one player which by default is game over
+	else{
+		return NULL;
+	}
+
+}
+
+
+
+
+//Reverses the order of the turn. Need to figure out how the order would be reversed
+UnoPlayer *UnoDeck::reverse(){
+
+	if(player->prev != player){
+		return player->prev;
+	}
+	else{
+		return NULL;
+	}
+
+}
+
+
+UnoCard *UnoDeck::deal(){
+	UnoCard *card = this->card;
+
+	if(card == NULL){
+		return NULL;
+	}
+
+	else{
+		this->hand->cardNum = 0;
+		UnoCard *back = card;
+
+		for(int i = 0; i < 7; ++i){
+			while(card != NULL){
+
+			}
+		}
+	}
+}
+
+
+//Skips the player and check to see if the next's next pointer is not null
+/*UnoCard *UnoDeck::skip(){
 	UnoCard *card = this->hand;
 
 	//Checks to see if there are players remaining in this case more than two players
@@ -36,7 +91,7 @@ UnoCard *UnoDeck::skip(){
 		card = card->next->next;
 	}
 	//Checks to see if there are two players left
-	else if(card->next->next == NULL){
+	else if(card->next->next == card->next){
 		card = this;
 	}
 	//If there is one player left(game would be over by the way)
@@ -47,6 +102,26 @@ UnoCard *UnoDeck::skip(){
 
 	return card;
 }
+
+
+
+
+//Reverses the turn order
+UnoCard *UnoDeck::reverse(){
+	UnoCard *card = this->hand;
+
+	//Checks to see if there is only one player
+	if(card->prev == NULL && card->next == NULL){
+		card = NULL;
+	}
+	//Else reverse the turn order
+	else{
+		card = card->prev;
+	}
+
+	return card;
+}
+*/
 
 //Gets the total amount of cards
 int UnoDeck::getTotalCards(){
@@ -59,18 +134,28 @@ int UnoDeck::getTotalDiscarded(){
 	return this->totalDiscarded;
 }
 
-
+//Shuffles the deck before dealing out the cards
 void UnoDeck::shuffle(){
-
+	int shuffle = rand % 108;
 }
 
-
+//In case a player has a wild card
 void UnoDeck::wild(string color){
 	this->hand->setColor(color);
+
 }
 
-//Reshuffles the deck when the totalCards are zero
+//Reshuffles the deck when the totalCards are zero and the pile is full
 void UnoDeck::reShuffle(){
+	int shuffle = rand % 108;
+	if(this->totalCards == 0 && this->totalDiscarded == 108){
+
+	}
+}
+
+void UnoDeck::pluckACard(UnoCard *card){
+	this->hand->cardNum += 1;
+	this->totalCards -=1;
 
 }
 
@@ -79,14 +164,14 @@ void UnoDeck::reShuffle(){
 void UnoDeck::drawTwo(){
 	this->hand->cardNum += 2;
 	this->totalCards -= 2;
-	skip();
+	skip(this->player);
 }
 
 void UnoDeck::drawFour(string color){
 	this->hand->cardNum += 4;
 	this->totalCards -= 4;
 	this->hand->setColor(color);
-	skip();
+	skip(this->player);
 }
 
 //Clears the deck object
